8dc66232123804f0527ebc839311f4931cc3241b
KotokD <kotok.daria@gmail.com>
2017-06-08 22:46:42 +0300

    some code changes

--- sandbox/src/main/java/pack/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pack/sandbox/MyFirstProgram.java
@@ -1,9 +1,7 @@
 package pack.sandbox;
 
-public class MyFirstProgram
-{
-public static void main(String[] args)
-{
-System.out.println("Hello Word");
-}
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Hello Word");
+  }
 }
